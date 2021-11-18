262d14d3cabd8f8678efcba53a37a8ca30c7e864
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2021-11-18 23:32:03 +0300

    6th Practice: λ-calculus imitation

---
+++ src/Practice/SxthPractice.hs
@@ -0,0 +1,15 @@
+module Practice.SxthPractice where
+
+-- Lambda Calculus imitation
+
+type VariableName = String
+
+data Expression
+  = Variable String
+  | Application Expression Expression
+  | Abstraction VariableName Expression
+
+instance Show Expression where
+  show (Variable a)      = a
+  show (Application a b) = show a ++ " " ++ show b
+  show (Abstraction a b) = "\\" ++ a ++ " -> " ++ show b
