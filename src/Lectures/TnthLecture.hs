8974c435eb7a53df661e0ecba41b6bb481113796
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2021-12-10 17:24:27 +0300

    10th Lecture & 10th Practice: Functor, Appllicative

---
+++ src/Lectures/TnthLecture.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE InstanceSigs #-}
+module Lectures.TnthLecture where
+
+import           Prelude hiding (Applicative, pure, (<*>))
+
+-- Композиция не коммутативна
+
+-- Чистота:
+
+-- - Определённость/вычислимость/завершимость
+
+--    ∃ y = f x
+--    counterexample: 2/0
+
+-- - Детерминированность
+
+--    f x = f x
+--    counterexample: random()
+--    Существует функция pseudorandom
+
+-- - Отсутсвие побочных эффектов
+
+-- Функтор - это морфизм, который сохраняет форму
+
+-- - Недетерминированность - неявная зависимость
+
+newtype Reader r a = R (r -> a)
+
+runReader :: Reader r a -> r -> a
+runReader (R f) = f
+
+isTemperatureGood :: Reader Integer Bool
+isTemperatureGood = R $ \t -> t > 0 && t < 30
+
+class Functor f => Applicative f where
+  pure :: a -> f a
+
+  -- | "ap(ply)"
+  (<*>) :: f (a -> b) -> f a -> f b
+  (<*>) = liftA2 id
+
+  liftA2 :: (a -> b -> c) -> f a -> f b -> f c
+  liftA2 f x y = pure f <*> x <*> y
+
+instance Functor (Reader r) where
+  fmap :: (a -> b) -> Reader r a -> Reader r b
+  fmap f (R g) = R $ f . g
+
+instance Applicative [] where
+  pure :: a -> [a]
+  pure x = [x]
+
+  fs <*> xs = [f x|f <- fs, x <- xs]
+
+isTemperatureBad :: Reader Integer Bool
+isTemperatureBad = not <$> isTemperatureGood
+
+-- Функции с названиями hoist promote lift отражают
+-- идею расширения контекста
---
+++ src/Practice/TnthPractice.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Practice.TnthPractice where
+
+import           Control.Applicative (Applicative (liftA2))
+
+newtype Identity a = I a
+  deriving (Show)
+
+instance Functor Identity where
+  fmap :: (a -> b) -> Identity a -> Identity b
+  fmap f (I x) = I $ f x
+
+instance Applicative Identity where
+  pure :: a -> Identity a
+  pure a = I a
+
+  (<*>) :: Identity (a -> b) -> Identity a -> Identity b
+  (<*>) (I f) (I x) = I $ f x
+
+-- -- $> fmap (+1) $ I 10
+
+-- -- $> (+1) <$> I 10
+
+-- -- $> (+) <$> I 10 <*> I 10
+
+-- -- $> I (+1) <*> I 10
+
+-- $> (fmap . fmap . fmap) (+1) [Just (I 10), Just (I 20), Nothing]
+
